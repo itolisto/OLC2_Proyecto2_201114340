a87c66818b237ea0acc008ac7c7b7c6835ae6fc3
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-21 08:13:57 -0600

    chore: add lexer

--- src/main/java/okik/tech/lexer/Lexer.java
+++ src/main/java/okik/tech/lexer/Lexer.java
@@ -33,4 +33,87 @@ public class Lexer {
         peek = ' ';
         return true;
     }
+
+    public Token scan() throws IOException {
+        for ( ; ; readch()) {
+            if (peek == ' ' || peek == '\t') continue;
+            else if (peek == '\n') line = line + 1;
+            else break;
+        }
+
+        switch(peek) {
+            case '&':
+                if (readch('&'))
+                    return Word.and;
+                else
+                    return new Token('&');
+            case '|':
+                if (readch('|'))
+                    return Word.or;
+                else
+                    return new Token('|');
+            case '=':
+                if (readch('='))
+                    return Word.eq;
+                else
+                    return new Token('=');
+            case '!':
+                if (readch('='))
+                    return Word.ne;
+                else
+                    return new Token('!');
+            case 'く':
+                if (readch('='))
+                    return Word.le;
+                else
+                    return new Token('<');
+
+            case '>':
+                if (readch('='))
+                    return Word.ge;
+                else
+                    return new Token('>');
+        }
+
+        if (Character.isDigit(peek)) {
+            int v = 0;
+            do {
+                v = 10 * v + Character.digit(peek, 10);
+                readch();
+            } while (Character.isDigit(peek));
+
+            if (peek != '.')
+                return new Num(v);
+
+            float x = v;
+            float d = 10;
+
+            for (; ; ) {
+                readch();
+                if (!Character.isDigit(peek)) break;
+                x = x + Character.digit(peek, 10) / d;
+                d = d * 10;
+            }
+            return new Real(x);
+        }
+
+        if(Character.isLetter(peek)) {
+            StringBuffer b = new StringBuffer();
+            do {
+                b.append(peek);
+                readch();
+            } while(Character.isLetterOrDigit(peek));
+
+            String s = b.toString();
+            Word w = words.get(s) ;
+            if (w != null) return w;
+            w = new Word(s, Tag.ID);
+            words.put(s, w);
+            return w;
+        }
+
+        Token tok = new Token(peek);
+        peek = ' ';
+        return tok;
+    }
 }
