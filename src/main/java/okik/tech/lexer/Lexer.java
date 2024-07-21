1ad9ef8637d3820c17150ee945a54b09ab70f5f8
Mauricio Flores <itolisto@gmail.com>
2024-07-21 08:58:41 -0600

    chore: add type

--- src/main/java/okik/tech/lexer/Lexer.java
+++ src/main/java/okik/tech/lexer/Lexer.java
@@ -13,7 +13,7 @@ public class Lexer {
     }
 
     public Lexer() {
-        reserve ( new Word("if", Tag. IF);
+        reserve ( new Word("if", Tag.IF) );
         reserve ( new Word("else", Tag.ELSE) );
         reserve ( new Word("while", Tag.WHILE) );
         reserve ( new Word ("do", Tag.D0) );
