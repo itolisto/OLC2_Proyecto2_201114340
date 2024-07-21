565c8304959c73076759321dd4e3a11f5ad6081d
Mauricio Flores <itolisto@gmail.com>
2024-07-21 09:04:05 -0600

    chore: add type

--- src/main/java/okik/tech/symbols/Type.java
+++ src/main/java/okik/tech/symbols/Type.java
@@ -17,4 +17,14 @@ public class Type extends Word {
             Char = new Type("char", Tag.BASIC, 1),
             Bool = new Type("bool", Tag.BASIC, 1);
 
+    public static boolean isNotNumeric(Type p) {
+        return p != Type.Char && p != Type.Int && p != Type.Float;
+    }
+
+    public static Type max(Type p1, Type p2) {
+        if (isNotNumeric(p1) || isNotNumeric(p2)) return null;
+        else if (p1 == Type.Float || p2 == Type.Float) return Type.Float;
+        else if (p1 == Type. Int || p2 == Type.Int) return Type. Int;
+        else return Type. Char;
+    }
 }
