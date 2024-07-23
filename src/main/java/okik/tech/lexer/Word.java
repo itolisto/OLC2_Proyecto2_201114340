cbb677d844b49b7971e0f2f8ed49b26c5cab7ff0
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-22 21:38:20 -0600

    chore: format word

--- src/main/java/okik/tech/lexer/Word.java
+++ src/main/java/okik/tech/lexer/Word.java
@@ -13,11 +13,11 @@ public class Word extends Token {
     }
 
     public static final Word
-            and = new Word ( "&&", Tag. AND ), or = new Word ("I|", Tag.OR ),
-            eq = new Word ("==", Tag.EQ), ne = new Word("!=", Tag. NE ),
-            le = new Word("<=", Tag. LE ), ge = new Word ("›=", Tag. GE),
+            and = new Word ( "&&", Tag.AND ), or = new Word ("||", Tag.OR ),
+            eq = new Word ("==", Tag.EQ), ne = new Word("!=", Tag.NE ),
+            le = new Word("<=", Tag.LE ), ge = new Word (">=", Tag. GE),
             minus = new Word ( "minus", Tag.MINUS ),
-            True = new Word( "true", Tag. TRUE ),
+            True = new Word( "true", Tag.TRUE ),
             False = new Word ( "false", Tag.FALSE ),
-            temp = new Word( "t", Tag. TEMP );
+            temp = new Word( "t", Tag.TEMP );
 }
