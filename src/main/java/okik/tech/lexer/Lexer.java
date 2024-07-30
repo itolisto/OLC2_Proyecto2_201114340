f5fa0b7c531f96439c44f3629b4a8db15ffb1653
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-29 22:59:30 -0600

    format: delete space

--- src/main/java/okik/tech/lexer/Lexer.java
+++ src/main/java/okik/tech/lexer/Lexer.java
@@ -17,9 +17,9 @@ public class Lexer {
         reserve(new Word("if", Tag.IF));
         reserve(new Word("else", Tag.ELSE));
         reserve(new Word("while", Tag.WHILE));
-        reserve(new Word ("do", Tag.DO));
-        reserve(new Word ("break", Tag.BREAK));
-        reserve ( Word.True ); reserve ( Word.False );
+        reserve(new Word("do", Tag.DO));
+        reserve(new Word("break", Tag.BREAK));
+        reserve(Word.True); reserve ( Word.False );
         reserve ( Type.Int ); reserve ( Type. Char );
         reserve ( Type.Bool ); reserve( Type.Float );
     }
