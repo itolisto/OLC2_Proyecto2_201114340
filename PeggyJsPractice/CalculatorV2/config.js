96a21a1636027eecdee821fd21280a3c58bc678d
Mauricio Flores <itolisto@gmail.com>
2024-08-04 22:26:19 -0600

    chore: comment

--- PeggyJsPractice/CalculatorV2/config.js
+++ PeggyJsPractice/CalculatorV2/config.js
@@ -2,3 +2,5 @@ module.exports = {
     format: "es",
     input: "./PeggyJsPractice/CalculatorV2/operationsV2.pegjs"
   };
+
+  // Generate translator with the following command: npx peggy -c ./PeggyJsPractice/CalculatorV2/config.js
