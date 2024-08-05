e836ab90f074843c8d62f9d16b30ddc6b63743e4
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-04 21:31:59 -0600

    chore: set up new calculator

---
+++ PeggyJsPractice/CalculatorV2.js
@@ -0,0 +1,30 @@
+import { parse } from './operationsV2.js'
+
+const imput = document.getElementById("input")
+const button = document.getElementById("btn")
+const pre = document.getElementById("ast")
+const output = document.getElementById("output")
+
+console.log('hello')
+
+const walkAST = (node) => {
+    if (node.type === 'number') return node.value
+
+    const num1 = walkAST(node.num1)
+    const num2 = walkAST(node.num2)
+
+    switch (node.type) {
+        case "sum":
+            return num1 + num2
+        case "multiplication":
+            return num1 * num2
+    }
+}
+
+button.addEventListener('click', () => {
+    const sourceCode = imput.value
+    const tree = parse(sourceCode)
+    ast.innerHTML = JSON.stringify(tree, null, 2)
+    const ouptupTree = walkAST(tree)
+    output.innerHTML = ouptupTree
+} )
