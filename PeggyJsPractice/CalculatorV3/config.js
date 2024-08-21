72d933b96004c0f145a845d36c542a620bf7a8eb
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-21 17:01:35 -0600

    chore: add new foler

---
+++ PeggyJsPractice/CalculatorV3/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    format: "es",
+    input: "./PeggyJsPractice/CalculatorV2/operationsV2.pegjs"
+  };
+
+  // Generate translator with the following command: npx peggy -c ./PeggyJsPractice/CalculatorV2/config.js
