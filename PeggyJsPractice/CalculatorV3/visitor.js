c63b03ecb6ac240c7d0bb878bf6fee99977c8622
Mauricio Flores <itolisto@gmail.com>
2024-08-21 23:06:02 -0600

    chore: create visitor

--- PeggyJsPractice/CalculatorV3/visitor.js
+++ PeggyJsPractice/CalculatorV3/visitor.js
@@ -1,22 +1,22 @@
-class BaseVisitor {
+export class BaseVisitor {
 
     visitExpression(node) {
-
+        throw new Error('visitExpression() not implemented');
     }
 
     visitBinaryExpresion(node) {
-        
+        throw new Error('visitBinaryExpresion() not implemented');
     }
     
     visitUnaryExpresion(node) {
-
+        throw new Error('visitUnaryExpresion() not implemented');
     }
 
     visitLiteralExpression(node) {
-
+        throw new Error('visitLiteralExpression() not implemented');
     }
 
     visitParenthesis(node) {
-
+        throw new Error('visitParenthesis() not implemented');
     }
 }
