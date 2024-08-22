3b884b6111ae1d36a858c768a4df026284e698ac
Mauricio Flores <itolisto@gmail.com>
2024-08-21 19:56:33 -0600

    chore: set up import

--- PeggyJsPractice/CalculatorV3/config.js
+++ PeggyJsPractice/CalculatorV3/config.js
@@ -1,6 +1,9 @@
 module.exports = {
     format: "es",
-    input: "./PeggyJsPractice/CalculatorV2/operationsV2.pegjs"
+    input: "./PeggyJsPractice/CalculatorV3/parserV3.pegjs",
+    dependencies: {
+      'nodes': "./PeggyJsPractice/CalculatorV3/nodes.js"
+    }
   };
 
   // Generate translator with the following command: npx peggy -c ./PeggyJsPractice/CalculatorV2/config.js
