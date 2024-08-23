bbe9c45ac40bd73a28083a65db8b9c323cd74bf3
Mauricio Flores <itolisto@gmail.com>
2024-08-22 20:02:42 -0600

    chore: add node

--- PeggyJsPractice/LanguageV4/config.js
+++ PeggyJsPractice/LanguageV4/config.js
@@ -6,4 +6,4 @@ module.exports = {
     }
   };
 
-  // Generate translator with the following command: npx peggy -c ./CalculatorV3/config.js
+  // Generate translator with the following command: npx peggy -c ./LanguageV4/config.js
