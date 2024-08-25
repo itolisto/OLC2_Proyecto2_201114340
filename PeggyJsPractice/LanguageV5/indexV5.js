63e94a18d40c156c68a3a0104f8a0d4b77e58601
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-25 00:38:29 -0600

    chore: handle lexical errors

--- PeggyJsPractice/LanguageV5/indexV5.js
+++ PeggyJsPractice/LanguageV5/indexV5.js
@@ -8,15 +8,20 @@ const output = document.getElementById("output")
 
 button.addEventListener('click', () => {
     const sourceCode = imput.value
-    const statements = parse(sourceCode)
-    ast.innerHTML = JSON.stringify(statements, null, 2)
-    
-    const interpreter = new InterpreterVisitor()
-    // const result = tree.accept(interpreter)
+    try {
+        const statements = parse(sourceCode)
+        ast.innerHTML = JSON.stringify(statements, null, 2)
+        
+        const interpreter = new InterpreterVisitor()
+        // const result = tree.accept(interpreter)
 
-    for (const statement of statements) {
-        statement.accept(interpreter);
+        for (const statement of statements) {
+            statement.accept(interpreter);
+        }
+
+        output.innerHTML = interpreter.output   
+    } catch (error) {
+        console.log(JSON.stringify(error, null, 2))
+        output.innerHTML = error.message + ' at line ' + error.location.start.line + ' column ' + error.location.start.column
     }
-
-    output.innerHTML = interpreter.output
 } )
