52bc25f1e108f15b5a533609045be6d060e358b4
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-25 00:39:04 -0600

    chore: set config file

--- PeggyJsPractice/LanguageV5/config.js
+++ PeggyJsPractice/LanguageV5/config.js
@@ -6,4 +6,4 @@ module.exports = {
     }
   };
 
-  // Generate translator with the following command: npx peggy -c ./LanguageV4/config.js
+  // Generate translator with the following command: npx peggy -c ./LanguageV5/config.js
