94981f3051bf93edbabf3012cae54522e14d7f00
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-31 15:15:38 -0600

    chore: implement get method

--- PeggyJsPractice/LanguageV6/instance.js
+++ PeggyJsPractice/LanguageV6/instance.js
@@ -2,12 +2,28 @@
 export class Instance {
 
     constructor(oakClass, ) {
+        // type nodes.ClassDeclaration
         this.oakClass = oakClass
+        // type. 
         this.properties = []
 
     }
 
     set(name, value) {
         this.properties[name] = value
+    }
+
+    get(name) {
+        if (this.properties.hasOwnProperty(name)) {
+            return this.properties[name]
+        }
+
+        const method = this.oakClass.searchMethod(nane)
+
+        if (method) {
+            return method.bind(this)
+        }
+
+        throw new Error('property not found: ${name}')
     }
 }
