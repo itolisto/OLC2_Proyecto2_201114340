c3b75e0082d68978607c31354e571141ad3bf886
Juan Gomez <juan.e.g.f@outlook.com>
2024-08-31 00:12:13 -0600

    chore: add declared class

--- PeggyJsPractice/LanguageV6/declared.js
+++ PeggyJsPractice/LanguageV6/declared.js
@@ -1,5 +1,6 @@
 import { Callable } from "./callable";
 import { Environment } from "./environment";
+import { ReturnException } from "./transfer";
 
 export class DeclaredFunction extends Callable {
     // param "closure" ia named like that because each instance of a closure is beingg wrapped inside an new instance of this clase
@@ -18,5 +19,26 @@ export class DeclaredFunction extends Callable {
 
     invoke({interpreter, args}) {
         const environment = new Environment(this.closure);
+        this.node.params.forEach((param, index) => {
+            environment.set(param, args[index])
+        });
+
+        const prevEnv = interpreter.environment
+        interpreter.environment = environment
+
+        try {
+            this.node.block.accept(interpreter)
+        } catch (error) {
+            interpreter.environment = prevEnv
+
+            if (error instanceof ReturnException) {
+                return error.value
+            }
+
+            // TODO: implement/handle the rest of flow control sentences here
+        }
+
+        interpreter.environment = prevEnv
+        return null
     }
 }
