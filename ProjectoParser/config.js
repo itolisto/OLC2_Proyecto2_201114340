9355e9a04929d43ff5b395fbc4a40f4337296491
Juan Gomez <juan.e.g.f@outlook.com>
2024-09-04 14:24:49 -0600

    chore: add comment

--- ProjectoParser/config.js
+++ ProjectoParser/config.js
@@ -2,8 +2,12 @@ module.exports = {
     format: "es",
     input: "./oakland.pegjs",
     dependencies: {
-      'node': "./node.js"
+      'node': "./oaknode.js"
     }
   };
 
-  // Generate translator with the following command: npx peggy -c ./config.js
+  /**
+   *  Generate translator with the following command: npx peggy -c ./config.js or if you can generate a parser that is compatible with
+   * javascript from peggy's website just enter your grammar and in  "Parser variable For CommonJS" enter "Window.peg", download 
+   * the ES6 module, ES6 is the new module system
+  */
