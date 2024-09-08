db323166aef7508bbce346b120aba1741afc396a
Juan Gomez <juan.e.g.f@outlook.com>
2024-09-08 01:46:20 -0600

    chore: implement invoke

--- ProjectoParser/declaredfunction.js
+++ ProjectoParser/declaredfunction.js
@@ -1,4 +1,7 @@
 import { Callable } from "./callable.js"
+import { Environment } from "./environment.js"
+import { OakError } from "./errors/oakerror.js"
+import errors from "./errors/transfer.js"
 
 export class DeclaredFunction extends Callable {
     // node is the function node (returnType, id, params, body), innerscope is a local environment with a reference to the parent
@@ -9,12 +12,40 @@ export class DeclaredFunction extends Callable {
     }
 
     arity() {
-        return this.node.lenght
+        return this.node.params.lenght
     }
 
-    // invoke({interpreter, args}) {
-    //     const prevEnv = interpreter.environment
-    //     interpreter.environment = this.innerScope
+    invoke({interpreter, callNode}) {
+        const prevEnv = interpreter.environment
+        interpreter.environment = new Environment(prevEnv)
 
-    // }
+        const args = callNode.args
+
+        if(!(args.lenght == this.arity())) throw new OakError(node.location, 'provide values for all args')
+
+        // 1. set arguments values to parameters
+        this.node.params.forEach((param, index) => {
+            
+            const arg = args[index].interpret(interpreter)
+
+            try {
+                if(param.type == args[index].type) interpreter.environment.set(param.id, value)   
+                else throw new OakError(node.location, 'arg ${arg} type should be ${param.type}')
+            } catch (error) {
+                console.log(error)
+                throw new OakError(node.location, 'invalid type')
+            }
+        })
+
+        // 2. excecute body
+        try {
+            this.node.body.forEach(statement => {
+                statement.interpret(interpreter)
+            })
+        } catch (error) {
+            if (error instanceof errors.Return) {
+                return error.node
+            }
+        }
+    }
  }
