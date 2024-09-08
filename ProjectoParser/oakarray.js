f0d2bb64f07f7c19196f0aa71985f82426596954
Mauricio Flores <itolisto@gmail.com>
2024-09-08 09:30:40 -0600

    chore: add check and properties

--- ProjectoParser/oakarray.js
+++ ProjectoParser/oakarray.js
@@ -1,19 +1,34 @@
+import { OakError } from "./errors/oakerror.js"
 
 
 export class OakArray {
-    constructor({type, size, deep, elements}) {
+    constructor({type, size, deep, value}) {
         this.type
         this.deep
-        this.elements
+        this.size
+        // value is elements
+        this.value = []
     }
 
     // TODO type checking here or in interpreter?
-    set(index, value) {
-        elements[index] = value
+    set(index, node, interpreter) {
+        if(index + 1 > size) {
+            // TODO do we throw an error?
+            throw new OakError(node.location, 'index out of bounds ')
+            return
+        }
+
+        elements[index] = node.interpret(interpreter)
         return elements[index]
     }
 
     get(index) {
+        if(index + 1 > size) {
+            // TODO do we throw an error?
+            throw new OakError(node.location, 'index out of bounds ')
+            return
+        }
+        
         return elements[index]
     }
 }
