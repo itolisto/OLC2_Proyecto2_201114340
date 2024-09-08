09dc42fdf6c385dce07c9f03f97b74308c3cb2af
Juan Gomez <juan.e.g.f@outlook.com>
2024-09-07 22:04:05 -0600

    chore: add function impl

---
+++ ProjectoParser/declaredfunction.js
@@ -0,0 +1,19 @@
+import { Callable } from "./callable.js"
+
+export class DeclaredFunction extends Callable {
+    // node is the function node (returnType, id, params, body), innerscope is a local environment with a reference to the parent
+    constructor({node, innerScope}) {
+        this.node = node
+        this.innerScope = innerScope
+    }
+
+    arity() {
+        return this.node.lenght
+    }
+
+    // invoke({interpreter, args}) {
+    //     const prevEnv = interpreter.environment
+    //     interpreter.environment = this.innerScope
+
+    // }
+ }
