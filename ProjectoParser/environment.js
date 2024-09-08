e33389d8ecde83da8aac9f9ce61e8e222915a110
Mauricio Flores <itolisto@gmail.com>
2024-09-07 19:04:44 -0600

    chore: add environment class

---
+++ ProjectoParser/environment.js
@@ -0,0 +1,10 @@
+export class Environment {
+    constructor(parent = undefined) {
+        this.parent = parent
+        this.values = {};
+    }
+
+    // set(name, value) {
+    //     this.values[name] = value
+    // }
+}
