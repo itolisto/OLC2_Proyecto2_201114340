7d61c8a8ea53fa4bf588d00b1f791136449666a9
Mauricio Flores <itolisto@gmail.com>
2024-09-07 23:14:40 -0600

    chore: rename property

--- ProjectoParser/declaredfunction.js
+++ ProjectoParser/declaredfunction.js
@@ -2,9 +2,10 @@ import { Callable } from "./callable.js"
 
 export class DeclaredFunction extends Callable {
     // node is the function node (returnType, id, params, body), innerscope is a local environment with a reference to the parent
-    constructor({node, innerScope}) {
+    constructor({node, outerScope}) {
+        super()
         this.node = node
-        this.innerScope = innerScope
+        this.outerScope = outerScope
     }
 
     arity() {
