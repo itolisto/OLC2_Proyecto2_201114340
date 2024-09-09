dff9f59aa9af5419bd706cb003d20926f275998e
Mauricio Flores <itolisto@gmail.com>
2024-09-09 16:42:09 -0600

    chore: fix reference issues

--- ProjectoParser/oakarray.js
+++ ProjectoParser/oakarray.js
@@ -10,29 +10,24 @@ export class OakArray {
         this.value = value
     }
 
-    int(type, size, value, interpreter) {
-
-    }
-
     // TODO type checking here or in interpreter?
     set(index, node, interpreter) {
-        if(index + 1 > size) {
+        if(index + 1 > this.size) {
             // TODO do we throw an error?
             throw new OakError(node.location, 'index out of bounds ')
             return
         }
 
-        elements[index] = node.interpret(interpreter)
-        return elements[index]
+        this.value[index] = node.interpret(interpreter)
+        return this.value[index]
     }
 
     get(index) {
-        if(index + 1 > size) {
+        if(index + 1 > this.size) {
             // TODO do we throw an error?
-            throw new OakError(node.location, 'index out of bounds ')
-            return
+            return undefined
         }
         
-        return elements[index]
+        return this.value[index]
     }
 }
