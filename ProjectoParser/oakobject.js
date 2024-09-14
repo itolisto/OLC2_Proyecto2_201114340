13f6be607b673881d50b1f1d61bfaf8e4ba96eae
Mauricio Flores <itolisto@gmail.com>
2024-09-14 13:36:41 -0600

    chore: create keys method

--- ProjectoParser/oakobject.js
+++ ProjectoParser/oakobject.js
@@ -1,3 +1,7 @@
+import { Callable } from "./callable.js";
+import { OakError } from "./errors/oakerror.js";
+import { Instance } from "./instance.js";
+import { OakArray } from "./oakarray.js";
 
 export class OakObject {
     constructor() {
@@ -7,3 +11,22 @@ export class OakObject {
     }
 }
 
+class ObjectKeys extends Callable {
+    arity() {
+        return 1
+    }
+
+    invoke({interpreter, args}) {
+        if(args.length != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected ' + this.arity()}`)
+          
+        const arg = args[0]
+
+        if(!(arg instanceof Instance)) throw new OakError(null, `Can't get properties of a not Struct type`)
+        
+        const props = arg.properties.map((prop) => prop.name)
+        const result = new OakArray({type: 'string', size: props.length, deep: 1, value: props})
+
+        console.log(result)
+        return result
+    }
+}
