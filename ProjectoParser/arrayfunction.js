dd041f9654f042896c8ac9b6ec7332e09a348e08
Mauricio Flores <itolisto@gmail.com>
2024-09-14 12:36:21 -0600

    chore: unwrapp value

--- ProjectoParser/arrayfunction.js
+++ ProjectoParser/arrayfunction.js
@@ -1,5 +1,7 @@
 import { Callable } from "./callable.js";
+import { OakConstant } from "./constant.js";
 import { OakError } from "./errors/oakerror.js";
+import nodes from "./oaknode.js"
 
 export class OakIndexOf extends Callable {
     constructor(array) {
@@ -12,11 +14,14 @@ export class OakIndexOf extends Callable {
     }
 
     invoke({interpreter, args}) {
-        if(args.lenght != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected ' + this.arity()}`)
-        const index = args[0].interpreter(interpreter)
+        if(args.length != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected ' + this.arity()}`)
+        const value = args[0].interpret(interpreter).value
         
-        console.log(this.array.value.IndexOf(index.value))
-        return this.array.value.IndexOf(index.value)
+        // const arrayValues = this.array.value.map((entry) => entry.value)
+        // const index = arrayValues.indexOf(value)
+        // const result = new nodes.Literal({type: 'int', value: index})
+        // console.log(result)
+        return result
     }
 
 }
