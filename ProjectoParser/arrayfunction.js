5e76454de1dab2e0ac1e76198b1278cc0f764eec
Mauricio Flores <itolisto@gmail.com>
2024-09-14 08:06:04 -0600

    chore: index of

--- ProjectoParser/arrayfunction.js
+++ ProjectoParser/arrayfunction.js
@@ -1,10 +1,22 @@
 import { Callable } from "./callable";
+import { OakError } from "./errors/oakerror";
 
-export class ArrayFunction extends Callable {
-
-    constructor(arity, invoke) {
-        this.invoke = invoke
-        this.arity = arity
+export class IndexOf extends Callable {
+    constructor(array) {
+        super()
+        this.array = array
     }
+
+    arity() {
+        return 1
+    }
+
+    invoke({interpreter, args}) {
+        if(args.lenght != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected' + this.arity()}`)
+        const index = args[0].interpreter(interpreter)
+        
+        return this.array.value.IndexOf(index.value)
+    }
+
 }
 
