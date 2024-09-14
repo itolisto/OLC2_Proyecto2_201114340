e6c101068a2612a97e4224a0944a4bf5986d74e9
Juan Gomez <juan.e.g.f@outlook.com>
2024-09-14 14:40:52 -0600

    chore: add wrap values in literal

--- ProjectoParser/oakobject.js
+++ ProjectoParser/oakobject.js
@@ -2,12 +2,17 @@ import { Callable } from "./callable.js";
 import { OakError } from "./errors/oakerror.js";
 import { Instance } from "./instance.js";
 import { OakArray } from "./oakarray.js";
+import nodes from "./oaknode.js"
 
 export class OakObject {
     constructor() {
         this.functions = {
             'keys': new ObjectKeys()
         }
+    }
+
+    getFunction(name) {
+        return this.functions[name]
     }
 }
 
@@ -19,11 +24,13 @@ class ObjectKeys extends Callable {
     invoke({interpreter, args}) {
         if(args.length != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected ' + this.arity()}`)
           
-        const arg = args[0]
+        const arg = args[0].interpret(interpreter)
 
         if(!(arg instanceof Instance)) throw new OakError(null, `Can't get properties of a not Struct type`)
         
-        const props = arg.properties.map((prop) => prop.name)
+        const props = Object.keys(arg.properties).map((prop) => new nodes.Literal({type: 'string', value: prop }))
+
+
         const result = new OakArray({type: 'string', size: props.length, deep: 1, value: props})
 
         console.log(result)
