6b21ab245ce6702fadfaeee23114f480a9426864
Mauricio Flores <itolisto@gmail.com>
2024-09-14 23:08:57 -0600

    refactor: use click listener

--- ProjectoParser/script.js
+++ ProjectoParser/script.js
@@ -5,7 +5,82 @@ var error = document.getElementById("error")
 var input = document.createElement('input');
 const codeArea = document.getElementById('area');
 const console = document.getElementById('consoleOutput')
+const ejecutar = document.getElementById('ejecutar')
+const abrir = document.getElementById('abrir')
+const reportes = document.getElementById('reportes')
+const archivo = document.getElementById('archivo')
 
+ejecutar.addEventListener('click', () => {
+    const sourceCode = codeArea.value
+    // try {
+        console.innerHTML = ""
+        const statements = parse(sourceCode)
+        // console.innerHTML = JSON.stringify(statements, null, 2)
+         // const result = tree.accept(interpreter)
+        const interpreter = new VisitorInterpreter()
+
+        for (const statement of statements) {
+            statement.interpret(interpreter)
+        }
+        
+        console.innerHTML = interpreter.output
+    // } catch (error) {
+    //     console.log(JSON.stringify(error, null, 2))
+    //     output.innerHTML = error.message + ' at line ' + error.location.start.line + ' column ' + error.location.start.column
+    // }
+})
+
+abrir.addEventListener('click', () => {
+    
+input.type = 'file';
+
+input.onchange = e => { 
+
+   // getting a hold of the file reference
+   var file = e.target.files[0]; 
+
+//    if(!(file.name.includes('.oak'))) {
+    errorMessage()
+   }
+
+   // setting up the reader
+   var reader = new FileReader();
+   reader.readAsText(file,'UTF-8');
+
+   // here we tell the reader what to do when it's done reading...
+   reader.onload = readerEvent => {
+      var content = readerEvent.target.result; // this is the content!
+      codeArea.textContent = content
+   }
+
+}
+
+input.click();
+})
+
+reportes.addEventListener('click', () => {
+    var x = document.getElementById("demoDrop");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+})
+
+archivo.addEventListener('click', () => { 
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+})
 
 // Example JS for handling tab switching, more functionality can be added
 document.querySelectorAll('.tab-button').forEach(button => {
@@ -33,82 +108,10 @@ document.addEventListener('DOMContentLoaded', function() {
     updateLineNumbers();
 });
 
-function myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-green";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className = 
-        x.previousElementSibling.className.replace(" w3-green", "");
-    }
-}
-
-function myDropFunc() {
-    var x = document.getElementById("demoDrop");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-green";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className = 
-        x.previousElementSibling.className.replace(" w3-green", "");
-    }
-}
-
-function abrirArchivo() {
-    
-input.type = 'file';
-
-input.onchange = e => { 
-
-   // getting a hold of the file reference
-   var file = e.target.files[0]; 
-
-   if(file.type != 'oak') {
-    errorMessage()
-   }
-
-   // setting up the reader
-   var reader = new FileReader();
-   reader.readAsText(file,'UTF-8');
-
-   // here we tell the reader what to do when it's done reading...
-   reader.onload = readerEvent => {
-      var content = readerEvent.target.result; // this is the content!
-      codeArea.textContent = content
-   }
-
-}
-
-input.click();
-}
-
 function errorMessage() {
     // Changing HTML to draw attention
     error.innerHTML = "<span style='color: red;'>"+
     "Select an .oak file"
 }
 
-function execute() {
-    
-    const sourceCode = codeArea.value
-    // try {
-        console.innerHTML = ""
-        const statements = parse(sourceCode)
-        // console.innerHTML = JSON.stringify(statements, null, 2)
-         // const result = tree.accept(interpreter)
-        const interpreter = new VisitorInterpreter()
-
-        for (const statement of statements) {
-            statement.interpret(interpreter)
-        }
-        
-        console.innerHTML = interpreter.output
-    // } catch (error) {
-    //     console.log(JSON.stringify(error, null, 2))
-    //     output.innerHTML = error.message + ' at line ' + error.location.start.line + ' column ' + error.location.start.column
-    // }
-}
 
