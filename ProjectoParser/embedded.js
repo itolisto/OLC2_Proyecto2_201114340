def2c7a7c516aa741b3fe003c36439420efe3d4e
Mauricio Flores <itolisto@gmail.com>
2024-09-14 18:18:51 -0600

    chore: create parseFloat

--- ProjectoParser/embedded.js
+++ ProjectoParser/embedded.js
@@ -29,6 +29,33 @@ export class ParseInt extends Callable {
     }
 }
 
+export class ParseFloat extends Callable {
+    arity() {
+        return 1
+    }
+
+    invoke({interpreter, args}) {
+        if(args.length != this.arity()) throw new OakError(null, `arguments ${args.lenght > this.arity() ? 'are greater than expected' : 'missing expected ' + this.arity()}`)
+          
+        const arg = args[0].interpret(interpreter)
+
+        if(!(arg instanceof nodes.Literal)) throw new OakError(null, `Can't get properties of a not Struct type`)
+
+        if(arg.type != 'string') throw new OakError(null, `Only string values can be parsed`)
+
+        const float = parseFloat(`${arg.value}`)
+
+        if(int == undefined) {
+            throw new OakError(null, `${arg.value} can not be parsed to Int`)
+        } else {
+            const result = new nodes.Literal({type: 'float', value: float})
+            console.log('parserInt')
+            console.log(result)
+            return result
+        }
+    }
+}
+
 export default {
     ParseInt
 }
