d7774017e5b36fc9dce392279800a16e29bfb7ad
Mauricio Flores <itolisto@gmail.com>
2024-09-14 21:36:09 -0600

    chore: open file function

--- ProjectoParser/script.js
+++ ProjectoParser/script.js
@@ -47,3 +47,29 @@ function myDropFunc() {
         x.previousElementSibling.className.replace(" w3-green", "");
     }
 }
+
+function abrirArchivo() {
+    var input = document.createElement('input');
+input.type = 'file';
+
+input.onchange = e => { 
+
+   // getting a hold of the file reference
+   var file = e.target.files[0]; 
+
+   if(file.type != 'oak') throw 
+
+   // setting up the reader
+   var reader = new FileReader();
+   reader.readAsText(file,'UTF-8');
+
+   // here we tell the reader what to do when it's done reading...
+   reader.onload = readerEvent => {
+      var content = readerEvent.target.result; // this is the content!
+      console.log( content );
+   }
+
+}
+
+input.click();
+}
