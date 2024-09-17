a92272a31e1ff6c82be7c7abb0cd349126b51e3a
Mauricio Flores <itolisto@gmail.com>
2024-09-16 22:24:57 -0600

    chore: add logic to print env

--- ProjectoParser/environment.js
+++ ProjectoParser/environment.js
@@ -1,5 +1,5 @@
 import { OakError } from "./errors/oakerror.js";
-import nodes from "."
+
 import { OakClass } from "./oakclass.js";
 import { OakArray } from "./oakarray.js";
 import { Callable } from "./callable.js";
@@ -56,51 +56,104 @@ export class Environment {
     }
 
     printTable(scope) {
-        const table = Object.entries(this.values).reduce(([key, value]) => {
-            if(value == undefined) return ''
-
-            // structs
-            if(value instanceof OakClass) {
-
-            }
-            
-            // Arrays
-            if(value instanceof OakArray) {
-                
-            }
-
-            // functions embedded and user defined
-            if(value instanceof Callable) {
-                if(value instanceof DeclaredFunction) { 
-                    const returnType = value.node.returnType
-                    let arrayLevel = ''
-
-                    if(returnType.arrayLevel > 0) {
-                        arrayLevel = "[]".repeat(returnType.arrayLevel)
-                    }
-
-                    const func = `method: ${key} return: ${returnType.type}${arrayLevel} parameters: ${value.arity()} scope: ${scope}\n`
-                    return func
-                } else {
-                    // else it must be an embedded function
+        const table = Object.entries(this.values).reduce((prev, [[key, value]]) => {
+            if(prev == undefined) {
+                // structs
+                if(value instanceof OakClass) {
+                    const oakClass = `class: ${key} type: ${value.type} properties: ${value.arity()} scope: ${scope}\n`
+                    return oakClass
                 }
-            }
+                
+                // Arrays
+                if(value instanceof OakArray) {
+                    const array = `array: ${key} type: ${value.type}${"[]".repeat(value.deep)} size: ${value.size} elements: ${value.value} scope: ${scope}\n`
+                    return array
+                }
 
-            // SDK classes
-            if(value instanceof SysClass) {
-                const constant = `System class name: ${key} type: ${key} properties: ${value.properties.length} functions: ${value.functions.length} scope: ${scope}\n`
-                return constant
-            }
+                // functions embedded and user defined
+                if(value instanceof Callable) {
+                    if(value instanceof DeclaredFunction) { 
+                        const returnType = value.node.returnType
+                        let arrayLevel = ''
 
-            // Constants only used in For Each statements
-            if(value instanceof OakConstant) {
-                const constant = `Literal name: ${key} type: ${value.type} value: ${value.value.value} scope: ${scope}\n`
-                return constant
-            }
+                        if(returnType.arrayLevel > 0) {
+                            arrayLevel = "[]".repeat(returnType.arrayLevel)
+                        }
 
-            // else is a Literal
-            const literal = `Literal name: ${key} type: ${value.type} value: ${value.value} scope: ${scope}\n`
-            return literal
+                        const func = `method: ${key} return: ${returnType.type}${arrayLevel} parameters: ${value.arity()} scope: ${scope}\n`
+                        return func
+                    } else {
+                        // else it must be an embedded function
+                        const func = `method: ${key} return: ${returnType.type}${arrayLevel} parameters: ${value.arity()} scope: ${scope}\n`
+                        return func
+                    }
+                }
+
+                // SDK classes
+                if(value instanceof SysClass) {
+                    const constant = `System class name: ${key} type: ${key} properties: ${value.properties.length} functions: ${value.functions.length} scope: ${scope}\n`
+                    return constant
+                }
+
+                // Constants only used in For Each statements
+                if(value instanceof OakConstant) {
+                    const constant = `Literal name: ${key} type: ${value.type} value: ${value.value.value} scope: ${scope}\n`
+                    return constant
+                }
+
+                // else is a Literal
+                const literal = `Literal name: ${key} type: ${value.type} value: ${value.value} scope: ${scope}\n`
+                return literal
+            } else {
+                
+                // structs
+                if(value instanceof OakClass) {
+                    const oakClass = `${prev}class: ${key} type: ${value.type} properties: ${value.arity()} scope: ${scope}\n`
+                    return oakClass
+                }
+                
+                // Arrays
+                if(value instanceof OakArray) {
+                    const array = `${prev}array: ${key} type: ${value.type}${"[]".repeat(value.deep)} size: ${value.size} elements: ${value.value} scope: ${scope}\n`
+                    return array
+                }
+
+                // functions embedded and user defined
+                if(value instanceof Callable) {
+                    if(value instanceof DeclaredFunction) { 
+                        const returnType = value.node.returnType
+                        let arrayLevel = ''
+
+                        if(returnType.arrayLevel > 0) {
+                            arrayLevel = "[]".repeat(returnType.arrayLevel)
+                        }
+
+                        const func = `${prev}method: ${key} return: ${returnType.type}${arrayLevel} parameters: ${value.arity()} scope: ${scope}\n`
+                        return func
+                    } else {
+                        // else it must be an embedded function
+                        const func = `${prev}method: ${key} return: ${returnType.type}${arrayLevel} parameters: ${value.arity()} scope: ${scope}\n`
+                        return func
+                    }
+                }
+
+                // SDK classes
+                if(value instanceof SysClass) {
+                    const constant = `${prev}System class name: ${key} type: ${key} properties: ${value.properties.length} functions: ${value.functions.length} scope: ${scope}\n`
+                    return constant
+                }
+
+                // Constants only used in For Each statements
+                if(value instanceof OakConstant) {
+                    const constant = `${prev}Literal name: ${key} type: ${value.type} value: ${value.value.value} scope: ${scope}\n`
+                    return constant
+                }
+
+                // else is a Literal
+                const literal = `${prev}Literal name: ${key} type: ${value.type} value: ${value.value} scope: ${scope}\n`
+                return literal
+
+            }
         },
         undefined
         )
