81c059223ac74b4cd88e6f74acfebcc5733fc79d
Mauricio Flores <itolisto@gmail.com>
2024-09-23 01:43:20 -0600

    chore: add script

--- assembly/indexV5.js
+++ assembly/indexV5.js
@@ -1,4 +1,3 @@
-import { InterpreterVisitor } from './interpreter.js'
 import { parse } from './assemblyCalculator.js'
 
 const imput = document.getElementById("input")
@@ -9,19 +8,9 @@ const output = document.getElementById("output")
 button.addEventListener('click', () => {
     const sourceCode = imput.value
     try {
-        const statements = parse(sourceCode)
-        ast.innerHTML = JSON.stringify(statements, null, 2)
+        ast.innerHTML = parse(sourceCode)
         
-        const interpreter = new InterpreterVisitor()
-        // const result = tree.accept(interpreter)
-
-        for (const statement of statements) {
-            statement.accept(interpreter);
-        }
-
-        output.innerHTML = interpreter.output   
     } catch (error) {
-        console.log(JSON.stringify(error, null, 2))
-        output.innerHTML = error.message + ' at line ' + error.location.start.line + ' column ' + error.location.start.column
+        
     }
 } )
