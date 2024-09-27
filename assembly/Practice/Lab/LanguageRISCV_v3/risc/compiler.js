ea49f6744cb38a1f150a006835d88df377fac8fa
Juan Gomez <juan.e.g.f@outlook.com>
2024-09-26 20:30:54 -0600

    bufix: substraction

--- assembly/Practice/Lab/LanguageRISCV_v3/risc/compiler.js
+++ assembly/Practice/Lab/LanguageRISCV_v3/risc/compiler.js
@@ -23,11 +23,11 @@ export class CompilerVisitor extends BaseVisitor {
 
     visitBinaryExpresion(node) {
         this.code.comment(`Operation ${node.op}`)
-        node.left.accept(this)
-        node.right.accept(this)
+        node.left.accept(this)  // left
+        node.right.accept(this) // left | right stacks right on top of left
 
-        this.code.popObject(R.T0)
-        this.code.popObject(R.T1)
+        this.code.popObject(R.T0) // pops right
+        this.code.popObject(R.T1) // pops left
 
         switch(node.op) {
             case '+': 
@@ -35,7 +35,7 @@ export class CompilerVisitor extends BaseVisitor {
                 this.code.push(R.T0)
                 break
             case '-': 
-                this.code.sub(R.T0, R.T0, R.T1)
+                this.code.sub(R.T0, R.T1, R.T0)
                 this.code.push(R.T0)
                 break
             case '*': 
