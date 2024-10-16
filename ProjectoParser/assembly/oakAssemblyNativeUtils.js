cd2810723f3754d1846759de81cc4a155195d4e9
Mauricio Flores <itolisto@gmail.com>
2024-10-16 13:20:50 -0600

    chore: add itoa

--- ProjectoParser/assembly/oakAssemblyNativeUtils.js
+++ ProjectoParser/assembly/oakAssemblyNativeUtils.js
@@ -34,87 +34,79 @@ export const concatString = (generator, rd = R.T0) => {
     generator.ret()
 }
 
-const itoa = (type, rd = R.A0) => {
-    switch(type) {
-        case 'int':
-            this.comment('Copy hp add to stack, intialize variables, and store sign')
-            // # store current heap pointer address to new space in stack
-            this.pushToStack(R.HP)
-            this.comment('copy number in question')
-            this.mv(R.A1, R.A0)
-            this.comment('this is the length counter, for convenience 0 counts as length 1')
-            this.li(R.A2, 0)
-            // this constant will be used to divide the number in question
-            this.li(R.A3, 10)
+const itoa = (generator, rd = R.A0) => {
+    generator.comment('Copy hp add to stack, intialize variables, and store sign')
+    // # store current heap pointer address to new space in stack
+    generator.pushToStack(R.HP)
+    generator.comment('copy number in question')
+    generator.mv(R.A1, R.A0)
+    generator.comment('generator is the length counter, for convenience 0 counts as length 1')
+    generator.li(R.A2, 0)
+    // generator constant will be used to divide the number in question
+    generator.li(R.A3, 10)
 
-            const getLength = this.getLabel('getNumberLength')
-            this.space()
-            this.bgez(R.A0, getLength)
-            this.comment('minus is 45 in ASCII')
-            this.li(R.A4, 45)
-            this.sb(R.A4, R.HP)
-            this.addi(R.HP, R.HP, 1)
-            this.comment('turn number into positive for convenience')
-            this.sub(R.A0, R.ZERO, R.A0)
-            this.mv(R.A1, R.A0)
+    const getLength = generator.getLabel('getNumberLength')
+    generator.space()
+    generator.bgez(R.A0, getLength)
+    generator.comment('minus is 45 in ASCII')
+    generator.li(R.A4, 45)
+    generator.sb(R.A4, R.HP)
+    generator.addi(R.HP, R.HP, 1)
+    generator.comment('turn number into positive for convenience')
+    generator.sub(R.A0, R.ZERO, R.A0)
+    generator.mv(R.A1, R.A0)
 
-            this.space()
-            this.addLabel(getLength)
+    generator.space()
+    generator.addLabel(getLength)
 
-            this.comment('store number without last digit, by dividing it by 10')
-            this.div(R.A4, R.A1, R.A3)
-        
-            this.space()
-            this.comment('if A4 == 0 means length is calculated, start saving digist, if not set next run')
-            const saveDigit = this.getLabel('saveDigitAsCharacter')
-            this.beqz(R.A4, saveDigit)
-            this.comment('set next run to calcucalte length')
-            this.comment('increment length by 1')
-            this.addi(R.A2, R.A2, 1)
-            this.comment('we move this just to be able to store first digit when all digits have been processed')
-            this.mv(R.A1, R.A4)
-            this.j(getLength)
+    generator.comment('store number without last digit, by dividing it by 10')
+    generator.div(R.A4, R.A1, R.A3)
 
-            this.space()
-            const nextCharacter = this.getLabel('getNextIntCharacter')
-            this.addLabel(nextCharacter)
-            this.comment('reduce the number, until reaching next digit')
-            this.div(R.A5, R.A1, R.A3)
+    generator.space()
+    generator.comment('if A4 == 0 means length is calculated, start saving digist, if not set next run')
+    const saveDigit = generator.getLabel('saveDigitAsCharacter')
+    generator.beqz(R.A4, saveDigit)
+    generator.comment('set next run to calcucalte length')
+    generator.comment('increment length by 1')
+    generator.addi(R.A2, R.A2, 1)
+    generator.comment('we move generator just to be able to store first digit when all digits have been processed')
+    generator.mv(R.A1, R.A4)
+    generator.j(getLength)
 
-            this.space()
-            this.beqz(R.A4, saveDigit)
-            this.comment('this runs if next item is not reache yet')
-            this.addi(R.A4, R.A4, -1)
-            this.mv(R.A1, R.A5)
-            this.j(nextCharacter)
+    generator.space()
+    const nextCharacter = generator.getLabel('getNextIntCharacter')
+    generator.addLabel(nextCharacter)
+    generator.comment('reduce the number, until reaching next digit')
+    generator.div(R.A5, R.A1, R.A3)
 
-            this.space()
-            this.addLabel(saveDigit)
-            this.comment('length 0 means all characters are stored')
-            this.addi(R.A2, R.A2, -1)
-            this.mv(R.A4, R.A2)
-            this.comment('get digit and it\'s ASCII value')
-            this.mul(R.A5, R.A5, R.A3)
-            this.sub(R.A1, R.A1, R.A5)
-            this.comment('48 ASCII is number 0')
-            this.addi(R.A1, R.A1, 48)
-            this.sb(R.A1, R.HP)
-            this.addi(R.HP, R.HP, 1)
-            this.mv(R.A1, R.A0)
-            this.bgez(R.A2, nextCharacter)
-            this.comment('end of string character')
-            this.sb(R.ZERO, R.HP)
-            this.addi(R.HP, R.HP, 1)
+    generator.space()
+    generator.beqz(R.A4, saveDigit)
+    generator.comment('generator runs if next item is not reache yet')
+    generator.addi(R.A4, R.A4, -1)
+    generator.mv(R.A1, R.A5)
+    generator.j(nextCharacter)
 
-            this.stackMimic.pushObject(undefined, 4, undefined, 'string')
-            break
-        case 'float':
-            break
-        case 'bool':
-            break
-    }
+    generator.space()
+    generator.addLabel(saveDigit)
+    generator.comment('length 0 means all characters are stored')
+    generator.addi(R.A2, R.A2, -1)
+    generator.mv(R.A4, R.A2)
+    generator.comment('get digit and it\'s ASCII value')
+    generator.mul(R.A5, R.A5, R.A3)
+    generator.sub(R.A1, R.A1, R.A5)
+    generator.comment('48 ASCII is number 0')
+    generator.addi(R.A1, R.A1, 48)
+    generator.sb(R.A1, R.HP)
+    generator.addi(R.HP, R.HP, 1)
+    generator.mv(R.A1, R.A0)
+    generator.bgez(R.A2, nextCharacter)
+    generator.comment('end of string character')
+    generator.sb(R.ZERO, R.HP)
+    generator.addi(R.HP, R.HP, 1)
 
-    return this.popObject(rd)
+    generator.stackMimic.pushObject(undefined, 4, undefined, 'string')
+
+    return generator.popObject(rd)
 }
 
 export const oakUtils = {
