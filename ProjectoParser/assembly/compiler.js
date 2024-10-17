550f2932451d03016ab35ed0c8f0fb347ef347d0
Juan Gomez <juan.e.g.f@outlook.com>
2024-10-16 23:32:26 -0600

    chore: add greater than or equals binary operator code

--- ProjectoParser/assembly/compiler.js
+++ ProjectoParser/assembly/compiler.js
@@ -1056,9 +1056,22 @@ export class OakCompiler extends BaseVisitor {
                 type = 'bool'
                 break
             }
-            case '>=' :
-                node = new nodes.Literal({type: 'bool', value:leftValue >= rightValue})
+            case '>=' :{
+                const trueLabel = this.generator.getLabel()
+                const endLabel = this.generator.getLabel()
+                this.generator.bge(R.T1, R.T0, trueLabel)
+                this.generator.comment('false')
+                this.generator.li(R.A0, 0)
+                this.generator.j(endLabel)
+                this.generator.addLabel(trueLabel)
+                this.generator.comment('true')
+                this.generator.li(R.A0, 1)
+                this.generator.addLabel(endLabel)
+                this.generator.comment('save boolean to stack')
+            
+                type = 'bool'
                 break
+            }
             case '&&' :
                 node = new nodes.Literal({type: 'bool', value:leftValue && rightValue})
                 break
