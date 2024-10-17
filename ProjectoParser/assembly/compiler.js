259e00b5a11ab26d98c555e2c9a19adcd88b5b56
Juan Gomez <juan.e.g.f@outlook.com>
2024-10-16 23:06:49 -0600

    chore: add not equal binary operator code

--- ProjectoParser/assembly/compiler.js
+++ ProjectoParser/assembly/compiler.js
@@ -1037,7 +1037,19 @@ export class OakCompiler extends BaseVisitor {
                 break
             }
             case '!=' : {
-                node = new nodes.Literal({type: 'bool', value:leftValue != rightValue})
+                const trueLabel = this.generator.getLabel()
+                const endLabel = this.generator.getLabel()
+                this.generator.bne(R.T1, R.T0, trueLabel)
+                this.generator.comment('false')
+                this.generator.li(R.A0, 0)
+                this.generator.j(endLabel)
+                this.generator.addLabel(trueLabel)
+                this.generator.comment('true')
+                this.generator.li(R.A0, 1)
+                this.generator.addLabel(endLabel)
+                this.generator.comment('save boolean to stack')
+            
+                type = 'bool'
                 break
             }
             case '<' :
