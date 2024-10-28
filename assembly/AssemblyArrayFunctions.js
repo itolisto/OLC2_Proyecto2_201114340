6fea4e1233baf63298daae9f23933a146337494a
Mauricio Flores <itolisto@gmail.com>
2024-10-28 15:49:25 -0600

    chore: add index of

--- assembly/AssemblyArrayFunctions.js
+++ assembly/AssemblyArrayFunctions.js
@@ -156,7 +156,7 @@ export class ArrayJoin extends AssemblyFunction  {
         compiler.generator.addUtil('ftoa')
 
         compiler.generator.comment('A1 will have type, -1 means strings and char, 0 means float, 1 boolean, 2 ints')
-        switch(args) {
+        switch(args[0][0].subtype) {
             case 'float':
                 compiler.generator.li(R.A1, 0)
                 break
@@ -197,6 +197,192 @@ export class ArrayLength extends AssemblyFunction  {
         compiler.generator.addUtil('concatStringUtil')
 
         compiler.generator.jal(this.label)
+
+        return compiler.generator.buildStackObject(undefined, 4, undefined, 'int')
+    } 
+}
+
+export class ArrayIndexOf extends AssemblyFunction  {
+    constructor(label) {
+        super(label)
+    }
+
+    declaration(generator) {
+        generator.comment('Parameters:')
+        generator.comment('A0 will have address of first item of array, at begining only')
+        generator.comment('A1 will have type, -1 means strings and char, 0 means float, 1 boolean, 2 ints')
+        generator.comment('A3 will have value being compared')
+        generator.addLabel(this.label)
+        generator.addi(R.SP, R.SP, -4)  // 0 index A0
+        generator.sw(R.A0, R.SP)
+        generator.comment('Temporarely store lenght and type in stack too')
+        generator.addi(R.SP, R.SP, -4) // 1 type A1
+        generator.sw(R.A1, R.SP)
+        generator.addi(R.A0, R.A0, -4)
+        generator.addi(R.SP, R.SP, -4)  // 2 length A2
+        generator.lw(R.A5, R.A0)
+        generator.sw(R.A5, R.SP)
+        generator.mv(R.A2, R.A5)
+        generator.addi(R.A0, R.A0, 4)
+        // generator.lw(R.A0, R.A0)
+        generator.comment('here we will be storing the address of the concatenated string')
+        generator.addi(R.SP, R.SP, -4) // 4 length regresive count A4
+        generator.mv(R.A4, R.A2)
+        generator.sw(R.A4, R.SP)
+        generator.space()
+
+        const loop = generator.getLabel('indexOfLoop')
+        const loadNextIteration = generator.getLabel('nextIteration')
+        const endLabel = generator.getLabel('indexOfEnd')
+        generator.addLabel(loop)
+        generator.comment('check type')
+        const floatBranch = generator.getLabel('compareFloat')
+        const stringCharBranch = generator.getLabel('compareStringChars')
+        const boolBranch = generator.getLabel('compareBoolean')
+        
+        generator.comment('need this to compare types')
+        generator.li(R.S2, 1)
+        generator.space()
+
+        generator.beqz(R.A1, floatBranch)
+        generator.space()
+
+        generator.bltz(R.A1, stringCharBranch)
+        generator.space()
+
+        generator.beq(R.A1, R.S2, boolBranch)
+        generator.lw(R.A0, R.A0)
+        const intEqual = generator.getLabel('indexOfequalsInt')
+        generator.beq(R.A0, R.A3, intEqual) 
+        generator.li(R.A0, -1)
+        generator.j(loadNextIteration)
+        generator.addLabel(intEqual)
+        generator.sub(R.A0, R.A2, R.A4)
+        generator.j(endLabel)
+        generator.space()
+
+        generator.addLabel(boolBranch)
+        generator.lw(R.A0, R.A0)
+        const falseBranch = generator.getLabel('indexOfBoolFalse')
+        generator.bne(R.A0, R.A3, falseBranch)
+        generator.sub(R.A0, R.A2, R.A4)
+        generator.j(endLabel)
+        generator.space()
+
+        generator.addLabel(falseBranch)
+        generator.li(R.A0, -1)
+        generator.j(loadNextIteration)
+        generator.space()
+
+        generator.addLabel(floatBranch)
+        generator.flw(R.FA2, R.A0)
+        generator.feqs(R.S2, R.FA2, R.FA3)
+        const floatTrue = generator.getLabel('equalsFloats')
+        generator.beqz(R.S2, floatTrue)
+        generator.li(R.A0, -1)
+        generator.j(loadNextIteration)
+        generator.addLabel(floatTrue)
+        generator.sub(R.A0, R.A2, R.A4)
+        generator.j(endLabel)
+        generator.space()
+
+        generator.addLabel(stringCharBranch)
+        const stringTrue = generator.getLabel('equalsString')
+        generator.beq(R.A0, R.A3, stringTrue)
+        generator.li(R.A0, -1)
+        generator.j(loadNextIteration)
+        generator.addLabel(stringTrue)
+        generator.sub(R.A0, R.A2, R.A4)
+        generator.j(endLabel)
+        generator.space()
+        
+        // generator.comment('length regresive count')
+        // generator.lw(R.A4, R.SP)
+        // generator.comment('concatenated string address')
+        // generator.addi(R.SP, R.SP, 4)
+        // generator.lw(R.A3, R.SP)
+        // generator.comment('length')
+        // generator.addi(R.SP, R.SP, 4)
+        // generator.lw(R.A2, R.SP)
+        // generator.addi(R.SP, R.SP, -4)
+        // generator.addi(R.SP, R.SP, -4)
+        // generator.space()
+
+        // generator.comment('means first iteration no need to make the second addition')
+        
+        // generator.beq(R.A4, R.A2, loadNextIteration)
+        // generator.mv(R.A5, R.A0)
+        // generator.mv(R.A0, R.A3)
+        // generator.mv(R.A1, R.A5)
+        // generator.jal('concatStringUtil')
+        // generator.space()
+
+        generator.addLabel(loadNextIteration)      
+        generator.comment('length regresive count')
+        generator.lw(R.A4, R.SP)
+        generator.addi(R.A4, R.A4, -1)
+        generator.sw(R.A4, R.SP)
+        generator.beqz(R.A4, endLabel)
+        generator.comment('length')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A2, R.SP)
+        generator.comment('type')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A1, R.SP)
+        generator.comment('index')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A0, R.SP)
+        generator.addi(R.A0, R.A0, 4)
+        generator.sw(R.A0, R.SP)
+        // generator.lw(R.A0, R.A0)
+        generator.space()
+        generator.comment('return to top of stack')
+        generator.addi(R.SP, R.SP, -12)
+        generator.j(loop)
+
+        generator.addLabel(endLabel)
+        generator.addi(R.SP, R.SP, 4)
+        generator.addi(R.SP, R.SP, 4)
+        generator.addi(R.SP, R.SP, 4)
+        generator.ret()
+     }
+
+    invoke(args, compiler) {
+        compiler.generator.comment('indexOf start')
+        compiler.generator.comment('A0 will have address of first item of array')
+        compiler.generator.comment('A1 will have type, -1 means strings and char, 0 means float, 1 boolean, 2 ints')
+
+        compiler.generator.newScope()
+        compiler.generator.comment('save array before interpreting args')
+        compiler.generator.pushObject('callee', args[0][0])
+        
+        args[1][0].interpret(compiler)
+        compiler.generator.mv(R.A3, R.A0)
+
+        compiler.generator.comment('find object in stack')
+        const object = compiler.generator.getMimicObject('callee')
+        compiler.generator.addi(R.SP, R.SP, -object.offset)
+        compiler.generator.lw(R.A0, R.SP)
+        compiler.generator.addi(R.SP, R.SP, object.offset)
+        compiler.generator.closeScope()
+
+        switch(args[0][0].subtype) {
+            case 'float':
+                compiler.generator.li(R.A1, 0)
+                break
+            case 'boolean':
+                compiler.generator.li(R.A1, 1)
+                break
+            case 'int':
+                compiler.generator.li(R.A1, 2)
+                break
+            default:
+                compiler.generator.li(R.A1, -1)
+                break
+        }
+
+        compiler.generator.jal(this.label)
+        compiler.generator.comment(`indexOf end`)
 
         return compiler.generator.buildStackObject(undefined, 4, undefined, 'int')
     } 
