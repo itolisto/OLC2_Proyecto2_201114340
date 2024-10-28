c14e2f46579b7db4f1f264bbe999e948edaf3f8a
Juan Gomez <juan.e.g.f@outlook.com>
2024-10-27 19:02:23 -0600

    chore: add interfaces to declare the sdk classes

---
+++ ProjectoParser/assembly/AssemblyClass.js
@@ -0,0 +1,30 @@
+export class AssemblyClass {
+
+    constructor(id, properties, functions) {
+        this.id = id,
+        this.properties = properties // properties is an object like { prop1: new StackObject(), prop2: new StackObject()}
+        this.functions = functions // functions of type AssemblyFunction
+    }
+
+    getFunction(id) {
+        throw new Error(`get function not implemented in ${this.id}`)
+    }
+
+    getProperty(id) {
+        throw new Error(`get property not implemented in ${this.id}`)
+    }
+}
+
+export class AssemblyFunction  {
+    constructor(label) {
+        this.label = label    
+    }
+
+    declaration(generator, params) {
+        throw new Error(`declaration not implemented in ${this.label} function`)
+    }
+
+    invoke(args, generator, compilerInterpreter) {
+        throw new Error(`declaration not implemented in ${this.label} function`)
+    } 
+}
