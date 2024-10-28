b096b89bdf669a7b91b62c3ba7fed9084dcab8bf
Mauricio Flores <itolisto@gmail.com>
2024-10-28 21:07:59 +0900

    Remover archivos innecesarios

--- PeggyJsPractice/CalculatorV2/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    format: "es",
-    input: "./PeggyJsPractice/CalculatorV2/operationsV2.pegjs"
-  };
-
-  // Generate translator with the following command: npx peggy -c ./PeggyJsPractice/CalculatorV2/config.js
--- PeggyJsPractice/LanguageV5/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    format: "es",
-    input: "./LanguageV5/parserV5.pegjs",
-    dependencies: {
-      'nodes': "./nodes.js"
-    }
-  };
-
-  // Generate translator with the following command: npx peggy -c ./LanguageV5/config.js
