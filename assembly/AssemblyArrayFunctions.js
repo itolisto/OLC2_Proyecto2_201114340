d6c1de4ac483178b91f4cb59be22424befbc4107
Mauricio Flores <itolisto@gmail.com>
2024-10-28 00:56:24 -0600

    chore: implement array join declaration

---
+++ assembly/AssemblyArrayFunctions.js
@@ -0,0 +1,133 @@
+import { OakGenerator } from "./generator.js"
+import { registers as R } from "./registers.js";
+
+export class ArrayJoin extends AssemblyFunction  {
+    constructor(label) {
+        super(label)
+    }
+
+    declaration() { 
+        const generator = new OakGenerator
+        generator.comment('Parameters:')
+        generator.comment('A0 will have address of first item of array, at begining only')
+        generator.comment('A1 will have type, -1 means strings and char, 0 means float, 1 boolean, 2 ints')
+        generator.addLabel(this.label)
+        generator.comment('Copy address of return and array first item in stack')
+        generator.add(R.SP, R.SP, -4)  // -1 return address
+        generator.sw(R.RA, R.SP)
+        generator.addi(R.SP, R.SP, -4)  // 0 index A0
+        generator.sw(R.A0, R.SP)
+        generator.comment('Temporarely store lenght and type in stack too')
+        generator.addi(R.SP, R.SP, -4) // 1 type A1
+        generator.sw(R.A1, R.SP)
+        generator.addi(R.A0, R.A0, -4)
+        generator.addi(R.SP, R.SP, -4)  // 2 length A2
+        generator.sw(R.A0, R.SP)
+        generator.mv(R.A2, R.A0)
+        generator.comment('here we will be storing the address of the concatenated string')
+        generator.addi(R.SP, R.SP, -4) // 3 address of concatenated string A3
+        generator.addi(R.SP, R.SP, -4) // 4 length regresive count A4
+        generator.mv(R.A4, R.A2)
+        generator.space()
+
+        const firstIteration = generator.getLabel('/joinFirstIteration')
+
+        const loop = generator.getLabel('/joinLoop')
+        generator.comment('check type')
+        const floatBranch = generator.getLabel('/jumpToFtoa')
+        const stringCharBranch = generator.getLabel('/joinConcat')
+        const boolBranch = generator.getLabel('/joinBoolean')
+        
+        generator.beqz(R.A1, floatBranch)
+        generator.space()
+
+        generator.bltz(R.A1, stringCharBranch)
+        generator.space()
+
+        generator.li(R.A3, 1)
+        generator.beq(R.A1, R.A3, boolBranch)
+        generator.comment('int to string')
+        generator.jal('itoa')
+        generator.space()
+
+        generator.addLabel(boolBranch)
+        generator.lw(R.A0, R.A0)
+        generator.comment('parse bool to string')
+        const falseBranch = generator.getLabel('/joinBoolFalse')
+        generator.beqz(R.A0, falseBranch)
+        generator.la(R.A0, 'true')
+        generator.j(stringCharBranch)
+        generator.space()
+
+        generator.addLabel(falseBranch)
+        generator.la(R.A0, 'false')
+        generator.j(stringCharBranch)
+        generator.space()
+
+        generator.addLabel(floatBranch)
+        // set itoa args
+        generator.jal('itoa')
+        generator.j(stringCharBranch)
+        generator.space()
+
+        generator.addLabel(stringCharBranch)
+        generator.la(R.A1, 'coma')
+        generator.jal('concatStringUtil')
+
+        generator.comment('length regreseive count')
+        generator.lw(R.A4, R.SP)
+        generator.comment('concatenated string address')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A3, R.SP)
+        generator.comment('length')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A2, R.SP)
+        generator.addi(R.SP, R.SP, -4)
+        generator.addi(R.SP, R.SP, -4)
+        generator.space()
+
+        generator.comment('means first iteration no need to make the second addition')
+        const fistIteration = generator.getLabel('joinFirstIteration')
+        generator.beq(R.A4, R.A2, firstIteration)
+        generator.mv(R.A5, R.A0)
+        generator.mv(R.A0, R.A3)
+        generator.mv(R.A1, R.A5)
+        generator.jal('concatStringUtil')
+        generator.space()
+
+        generator.addLabel(firstIteration)
+        const endLabel = generator.getLabel('/joinEnd')
+        generator.comment('length regreseive count')
+        generator.lw(R.A4, R.SP)
+        generator.addi(R.A4, R.A4, -1)
+        generator.beqz(R.A2, endLabel)
+        generator.comment('concatenated string address')
+        generator.addi(R.SP, R.SP, 4)
+        generator.sw(R.A0, R.SP)
+        generator.mv(R.A3, R.A0)
+        generator.comment('length')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A2, R.SP)
+        generator.comment('type')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A1, R.SP)
+        generator.comment('index')
+        generator.addi(R.SP, R.SP, 4)
+        generator.lw(R.A0, R.SP)
+        generator.addi(R.A0, R.A0, 4)
+        generator.space()
+
+
+        generator.addLabel(endLabel)
+     }
+
+    invoke(args, compiler) {
+        compiler.generator.comment(`Printing start`)
+            args.forEach((arg) => {
+                const input = arg.interpret(compiler)
+                compiler.generator.printInput(input.type)
+            })
+
+        compiler.generator.comment(`Printing end`)
+    } 
+}
